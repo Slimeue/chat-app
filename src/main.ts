a9fc763416798533f9dca7b7f0d08dd6a4fb7b11
Kevinroy <sc.kevinroy@gmail.com>
2024-04-27 05:14:42 +0800

    chore

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { graphqlUploadExpress } from 'graphql-upload';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('ChatApp');
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(6060);
+
+  logger.log(`Running on http://localhost:6060/graphql`);
 }
 bootstrap();
